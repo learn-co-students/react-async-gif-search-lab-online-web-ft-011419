902d9b3a91d876faef285d317d2dd68f6b3b08a3
Jared Harbison <jared.harbison@gmail.com>
2019-06-04 11:45:03 -0400

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+export default class GifSearch extends React.Component {
+  state = {
+    search: []
+  }
+  handleChange = event => {
+    this.setState({ search: event.target.value })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.searchHandler(this.state.search)
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' id='search' value={this.state.search} onChange={this.handleChange} />
+      </form>
+    )
+  }
+}
